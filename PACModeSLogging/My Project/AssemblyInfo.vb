f332cefffa4392dd394f0dd698729ff00b54c2cb
leonloberman <leon@loberman.net>
2021-04-29 15:37:00 +0100

    Revert "Built as v1.3.29.0 to test AutoUpdate function"
    This reverts commit ea0a8920521b1072085d30a57e47e199699d4bb3.

--- PACModeSLogging/My Project/AssemblyInfo.vb
+++ PACModeSLogging/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.3.29.0")>
-<Assembly: AssemblyFileVersion("1.3.29.0")>
+<Assembly: AssemblyVersion("1.3.28.9")>
+<Assembly: AssemblyFileVersion("1.3.28.9")>
